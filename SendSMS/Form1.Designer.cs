fa4054ceb4196c52c239537c25ab6ace49636bcc
Ahmed Khalid <a.k.kadhim@outloo.com>
2021-02-10 18:37:52 +0300

    Add project files.

---
+++ SendSMS/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SendSMS
+{
+    partial class SendSMS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.statusTB = new System.Windows.Forms.TextBox();
+            this.portsCB = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.portToggleBtn = new System.Windows.Forms.Button();
+            this.testBtn = new System.Windows.Forms.Button();
+            this.directCommandTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(186, 152);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(64, 47);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(197, 20);
+            this.textBoxNumber.TabIndex = 1;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(64, 73);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(197, 73);
+            this.textBoxMessage.TabIndex = 2;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(9, 178);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(41, 13);
+            this.labelStatus.TabIndex = 3;
+            this.labelStatus.Text = "status:";
+            // 
+            // statusTB
+            // 
+            this.statusTB.Location = new System.Drawing.Point(12, 194);
+            this.statusTB.Multiline = true;
+            this.statusTB.Name = "statusTB";
+            this.statusTB.ReadOnly = true;
+            this.statusTB.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.statusTB.Size = new System.Drawing.Size(249, 192);
+            this.statusTB.TabIndex = 4;
+            this.statusTB.WordWrap = false;
+            // 
+            // portsCB
+            // 
+            this.portsCB.FormattingEnabled = true;
+            this.portsCB.Location = new System.Drawing.Point(64, 20);
+            this.portsCB.Name = "portsCB";
+            this.portsCB.Size = new System.Drawing.Size(116, 21);
+            this.portsCB.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Message";
+            // 
+            // portToggleBtn
+            // 
+            this.portToggleBtn.Location = new System.Drawing.Point(186, 18);
+            this.portToggleBtn.Name = "portToggleBtn";
+            this.portToggleBtn.Size = new System.Drawing.Size(75, 23);
+            this.portToggleBtn.TabIndex = 9;
+            this.portToggleBtn.Text = "Connect";
+            this.portToggleBtn.UseVisualStyleBackColor = true;
+            this.portToggleBtn.Click += new System.EventHandler(this.portToggleBtn_Click);
+            // 
+            // testBtn
+            // 
+            this.testBtn.Location = new System.Drawing.Point(12, 152);
+            this.testBtn.Name = "testBtn";
+            this.testBtn.Size = new System.Drawing.Size(75, 23);
+            this.testBtn.TabIndex = 10;
+            this.testBtn.Text = "Test";
+            this.testBtn.UseVisualStyleBackColor = true;
+            this.testBtn.Click += new System.EventHandler(this.testBtn_Click);
+            // 
+            // directCommandTB
+            // 
+            this.directCommandTB.Location = new System.Drawing.Point(12, 126);
+            this.directCommandTB.Name = "directCommandTB";
+            this.directCommandTB.Size = new System.Drawing.Size(41, 20);
+            this.directCommandTB.TabIndex = 11;
+            // 
+            // SendSMS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 401);
+            this.Controls.Add(this.directCommandTB);
+            this.Controls.Add(this.testBtn);
+            this.Controls.Add(this.portToggleBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.portsCB);
+            this.Controls.Add(this.statusTB);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.buttonSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "SendSMS";
+            this.Text = "Send SMS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.TextBox statusTB;
+        private System.Windows.Forms.ComboBox portsCB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button portToggleBtn;
+        private System.Windows.Forms.Button testBtn;
+        private System.Windows.Forms.TextBox directCommandTB;
+    }
+}
+
---
+++ SendSMS/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SendSMS
+{
+    public partial class SendSMS : Form
+    {
+        private SerialPort _serialPort;
+        private bool portOpened;
+        private List<string> commands = new List<string>();
+        private int commandsPointer = 0;
+        private bool completed = false;
+
+        public SendSMS()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //textBoxNumber.Text = "+9647830707775";
+            //textBoxMessage.Text = "Hi";
+            string[] ports = SerialPort.GetPortNames();
+            portsCB.DataSource = ports;
+            SetPortState(false);
+            FillCommands();
+        }
+
+        private void FillCommands()
+        {
+            //commands.Add("AT\r");                   //0 - Once the handshake test is successful, it will back to OK
+            //commands.Add("AT+CSQ\r");               //1- Signal quality test, value range is 0-31 , 31 is the best
+            //commands.Add("AT+CCID\r");              //2- Read SIM information to confirm whether the SIM is plugged
+            //commands.Add("AT+CREG?\r");             //3- Check whether it has registered in the network
+            commands.Add("AT+CSCS=\"UCS2\"\r");      //4- displays the codepages supported by the modem (UCS2,HEX) SMS doesn’t effected by this command
+            commands.Add("AT+CMGF=1\r");            //6- Configuring TEXT mode, other option could not send mobile terminated message (PDU Mode)
+            commands.Add("AT+CSMP=17,167,0,8\r");   //5- specify the correct DCS (Data Coding Scheme) for Unicode messages
+            commands.Add("AT+CMGS=");               //7- change with country code and xxxxxxxxxxx with phone number to sms
+            //commands.Add("message content");//text content
+        }
+
+        private void SetPortState(bool state)
+        {
+            portOpened = state;
+            portToggleBtn.Text = state ? "Disconnect" : "Connect";
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            completed = false;
+            buttonSend.Enabled = false;
+            SendMessage();
+        }
+
+        private void SendMessage()
+        {
+            if (portOpened)
+            {
+                if (commandsPointer == (commands.Count - 1))
+                {
+                    string number = "\"" + textBoxNumber.Text + "\",145\r";
+                    _serialPort.Write(commands[commandsPointer] + number);
+                }
+                else if (commandsPointer == commands.Count)
+                {
+                    commandsPointer++;
+                    completed = true;
+                    string message = textBoxMessage.Text;
+                    //_serialPort.Write(message + (char)(26));
+                    //string msg = Str2Hex("مرحبا");
+                    //message = Encoding.BigEndianUnicode.GetString(Encoding.Default.GetBytes(message));
+                    //_serialPort.Encoding = Encoding.Unicode;
+                    _serialPort.Write(message + "\x1A");
+                    //byte[] ba = Encoding.Unicode.GetBytes(message);
+                   // _serialPort.Write(new byte[] { 0xFE, 0xA5 },0,2);
+                    _serialPort.Write("\x1A");
+                    _serialPort.Encoding = Encoding.ASCII;
+                    //06450631062D06280627
+                }
+                else
+                {
+                    _serialPort.Write(commands[commandsPointer]);
+                }
+                commandsPointer++;
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                MessageBox.Show("Port must be open");
+                commandsPointer = 0;
+                buttonSend.Enabled = true;
+            }
+        }
+
+        public static string Str2Hex(string strMessage)
+        {
+            byte[] ba = Encoding.BigEndianUnicode.GetBytes(strMessage);
+            string strHex = BitConverter.ToString(ba);
+            strHex = strHex.Replace("-", "");
+            return strHex;
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            String data = _serialPort.ReadExisting();
+            if (statusTB.InvokeRequired)
+            {
+                statusTB.BeginInvoke((MethodInvoker)delegate () { this.statusTB.AppendText(data);});
+            }
+            else
+            {
+                statusTB.Text = _serialPort.ReadExisting();
+            }
+
+            if (commandsPointer >= (commands.Count + 1) || completed)
+            {
+                commandsPointer = 0;
+                if (buttonSend.InvokeRequired)
+                {
+                    buttonSend.BeginInvoke((MethodInvoker)delegate () { this.buttonSend.Enabled = true; });
+                }
+                else
+                {
+                    buttonSend.Enabled = true;
+                }
+            }
+            else
+            {
+                SendMessage();
+            }
+        }
+
+        private void portToggleBtn_Click(object sender, EventArgs e)
+        {
+            commandsPointer = 0;
+            SetPortState(!portOpened);
+            if (portOpened)
+            {
+                if (_serialPort != null && _serialPort.IsOpen)
+                {
+                    _serialPort.Close();
+                }
+                _serialPort = new SerialPort();
+                _serialPort.PortName = portsCB.Text;
+                //_serialPort.Encoding = Encoding.BigEndianUnicode;
+                _serialPort.BaudRate = 115200;
+                _serialPort.Parity = System.IO.Ports.Parity.None;
+                _serialPort.DataBits = 8;
+                _serialPort.StopBits = System.IO.Ports.StopBits.One;
+                _serialPort.Handshake = System.IO.Ports.Handshake.None;
+                _serialPort.RtsEnable = true;
+                _serialPort.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+                Thread.Sleep(1000);
+                _serialPort.Open();
+                if (_serialPort.IsOpen)
+                {
+                    MessageBox.Show("Port opened");
+                }
+            }
+            else
+            {
+                if (_serialPort != null && _serialPort.IsOpen)
+                {
+                    _serialPort.Close();
+                    if (_serialPort.IsOpen == false)
+                    {
+                        MessageBox.Show("Port cloased");
+                    }
+                }
+            }
+        }
+
+        private void testBtn_Click(object sender, EventArgs e)
+        {
+            if (portOpened)
+            {
+                if (string.IsNullOrEmpty(directCommandTB.Text))
+                {
+                    //_serialPort.Write("AT\r");
+                    _serialPort.WriteLine("AT+CMGF=1\r");
+             
+                    //_serialPort.Write("AT+CSCS=?\r");
+                    completed = true;
+                }
+                else
+                {
+                    byte[] ba = Encoding.Unicode.GetBytes(directCommandTB.Text);
+                    //_serialPort.Encoding = Encoding.BigEndianUnicode;
+                    //_serialPort.Write(directCommandTB.Text + "\r");
+                    //_serialPort.Encoding = Encoding.ASCII;
+                    //_serialPort.Write(char.ConvertFromUtf32(26));
+                    completed = true;
+
+                    //directCommandTB.Text = Str2Hex("السلام");
+                }
+            }
+        }
+    }
+}
+
+
+//AT
+
+//+CME ERROR:58
+//AT+CSQ
+
+//+CSQ: 25,99
+
+//OK
+//AT+CCID
+
+//+SCID: SIM Card ID: 8996405605084009552F
+
+//OK
+//AT+CREG?
+
+//+CREG: 1,1
+
+//OK
+//AT+CMGF=1
+
+//OK
+//AT+CMGS=+9647738467559
+
+//> Alroia
+//+CMGS: 1
+
+//OK
